455d347ec7106b66b421c393f8eaabc76578f73d
WarisaTT <warisa.thia@mail.kmutt.ac.th>
2024-08-20 20:59:55 +0700

    Demo integrate Pat

--- src/App.vue
+++ src/App.vue
@@ -2,53 +2,202 @@
 import playButton from "./components/icons/play.png";
 import bulb from "./components/icons/bulb.png";
 import prize from "./components/icons/prize.png";
+import back from "./components/icons/back.png";
+import homeButton from "./components/icons/HomeButton.png";
+import helpButton from "./components/icons/helpButton.png";
+import soundButton from "./components/icons/soundButton.png";
+import loadSuccess from "./components/icons/loadPhoto.png";
+import levelSuccess from "./components/icons/level-up-photo.png";
+import prizePhoto from "./components/icons/prizePhoto.png";
+import Questions from '../Questions.json'
 
-import {ref} from 'vue'
+
+import { ref } from 'vue'
 
 const isVisible = ref(0)
+const level = ref(0)
+const selectedWord = ref([])
+const selectedAnswer = ref([])
+const spaceCount = ref(0)
+const count = ref(0)
+const correctAnswer = ref([])
+const col = ref(0)
 
-const modePage=()=>{
-  isVisible.value=1
+const modePage = () => {
+  isVisible.value = 1
 }
+
+const startPage = () => {
+  isVisible.value = 0
+}
+
+const easyPage = () => {
+  isVisible.value = 2
+}
+
+const successPage = () => {
+  isVisible.value = 3
+}
+
+const playMode = (difficulty) => {
+  isVisible.value = 2
+  const question = Questions.filter((q) => q.difficulty === difficulty)[
+    level.value
+  ]
+  selectedWord.value = question.word.split('')
+  spaceCount.value = selectedWord.value.length
+  col.value = selectedWord.value.length
+  correctAnswer.value = question.correctAnswer.split('')
+  selectedAnswer.value = Array(spaceCount.value).fill(' ')
+
+  console.log(selectedAnswer.value)
+}
+
+const playEasyMode = () => playMode('easy')
+const playMediumMode = () => playMode('medium')
+const playHardMode = () => playMode('hard')
+
+const selectLetter = (letter) => {
+  if (count.value < selectedAnswer.value.length) {
+    selectedAnswer.value[count.value] = letter
+    count.value += 1
+
+    if (count.value >= correctAnswer.value.length) {
+      checkAnswer()
+    }
+  }
+}
+
+const checkAnswer = () => {
+  const isCorrect = correctAnswer.value.every(
+    (char, index) => char === selectedAnswer.value[index]
+  )
+
+  if (isCorrect) {
+    console.log('you win')
+    // correctPage();
+  } else {
+    console.log('you lose')
+  }
+}
+
+function shuffleArray(arr) {
+  let shuffled = arr.slice() // Create a copy of the array
+  for (let i = shuffled.length - 1; i > 0; i--) {
+    const j = Math.floor(Math.random() * (i + 1))
+      ;[shuffled[i], shuffled[j]] = [shuffled[j], shuffled[i]] // Swap elements
+  }
+  return shuffled
+}
+
 </script>
 
 <template>
   <div>
     <!-- Start Page -->
-    <div v-if="isVisible===0" class="bg-[#FEF9EF] flex flex-col items-center justify-center h-screen">
+    <div v-if="isVisible === 0" class="bg-[#FEF9EF] flex flex-col items-center justify-center h-screen">
       <h1 class="text-[150px] justify-start text-[#237C9D] mt-[-75px]">CLICK WORD</h1>
-      <button @click="modePage"><img :src="playButton" alt="Play Button" class="w-60 h-60 mx-auto mb-[50px] mt-10 hover:scale-110"></button>
+      <button @click="modePage"><img :src="playButton" alt="Play Button"
+          class="w-60 h-60 mx-auto mb-[50px] mt-10 hover:scale-110"></button>
       <div class="flex gap-80 mt-10">
         <div class="flex flex-col item-center gap-2">
-          <img :src="prize" alt="Play Button" class="w-20 h-20 mx-auto my-auto mb-1 ">
+          <img :src="prize" alt="Prize" class="w-20 h-20 mx-auto my-auto mb-1 ">
           <h3 class="bg-[#19C3B2] text-[#FEF9EF] text-[20px] rounded-2xl p-3">Success (0%)</h3>
         </div>
         <div class="flex flex-col item-center gap-2">
-          <img :src="bulb" alt="Play Button" class="w-20 h-20 mx-auto my-auto mb-1 mr-3 ">
+          <img :src="bulb" alt="Bulb Button" class="w-20 h-20 mx-auto my-auto mb-1 mr-3 ">
           <h3 class="bg-[#FF9090] text-[#FEF9EF] text-[20px] rounded-2xl p-3">Hints (3)</h3>
         </div>
       </div>
     </div>
 
     <!-- Mode Page -->
-    <div v-if="isVisible===1" class="bg-[#FEF9EF] flex flex-col items-center justify-center h-screen gap-20">
-      <h1 class="text-[130px] justify-start text-[#237C9D] mt-[-200px]">MODE</h1>
-      <div class="flex flex-col gap-6 ">
-        <button
+    <div v-if="isVisible === 1" class="bg-[#FEF9EF] flex flex-col items-center justify-center h-screen gap-16">
+      <h1 class="text-[130px] justify-start text-[#237C9D] ">
+        MODE
+      </h1>
+      <div class="flex flex-col gap-6">
+        <button @click="playEasyMode"
           class="bg-[#19C3B2] text-[#FEF9EF] text-[40px] rounded-2xl px-8 hover:scale-110 hover:bg-[#20a396]">Easy</button>
-        <button
-          class="bg-[#FFCB77] text-[#FEF9EF] text-[40px] rounded-2xl px-8 hover:scale-110 hover:bg-[#ffb031]">Medium</button>
-        <button
-          class="bg-[#FE6D73] text-[#FEF9EF] text-[40px] rounded-2xl px-8 hover:scale-110 hover:bg-[#ee464c]">Hard</button>
+        <button @click="playMediumMode"
+          class="bg-[#FFCB77] text-[#FEF9EF] text-[40px] rounded-2xl px-8 hover:scale-110 hover:bg-[#ffb031]">
+          Medium
+        </button>
+        <button @click="playHardMode"
+          class="bg-[#FE6D73] text-[#FEF9EF] text-[40px] rounded-2xl px-8 hover:scale-110 hover:bg-[#ee464c]">
+          Hard
+        </button>
+        <button @click="startPage"><img :src="back" alt="Back Button"
+            class="w-20 h-20 mx-auto my-auto mb-1 mt-16 hover:scale-150"></button>
       </div>
     </div>
 
     <!-- Play Page -->
-    <div v-if="isVisible===2">
+    <div v-if="isVisible === 2" class="flex flex-col justify-between bg-[#FEF9EF] h-screen">
+      <div class="flex justify-between items-start">
+        <button @click="modePage">
+          <img :src="homeButton" alt="Home Button" class="w-[50px] h-[50px] ml-5 mt-5 hover:scale-110">
+        </button>
+        <h3 class="mt-6 text-4xl text-black">Level 1</h3>
+        <div class="flex flex-col">
+          <button @click="successPage">
+            <img :src="helpButton" alt="Help Button" class="w-[50px] h-[50px] mr-5 mt-5 hover:scale-110">
+          </button>
+          <button @click="modePage">
+            <img :src="soundButton" alt="Sound Button" class="w-[50px] h-[50px] mr-5 mt-5 hover:scale-110">
+          </button>
+        </div>
+      </div>
 
+      <div class="flex flex-col items-center justify-center flex-1 gap-8">
+        <div class="flex flex-row gap-2 mb-8">
+          <button v-for="word in selectedWord" :key="word" @click="selectLetter(word)"
+            class="text-[40px] text-[#FEF9EF] rounded-2xl w-20 h-20 bg-[#19C3B2]">
+            {{ word }}
+          </button>
+        </div>
+        <div class="flex flex-row gap-2 mb-8">
+          <div v-for="item in selectedAnswer" :key="item"
+            class="text-[40px] text-black flex justify-center items-center w-20 h-20 border-2 border-[#19C3B2] rounded-2xl">
+            {{ item }}
+          </div>
+        </div>
+      </div>
+
+      <div class="flex justify-center items-end mb-16 gap-10">
+        <button
+          class="bg-[#000000] text-[#FEF9EF] text-3xl rounded-xl px-8 w-56 hover:bg-[#878787] focus:bg-black">Clear</button>
+        <button
+          class="bg-[#000000] text-[#FEF9EF] text-3xl rounded-xl px-8 w-56 hover:bg-[#878787] focus:bg-black">Hints ( 3
+          )</button>
+      </div>
     </div>
+
+    <!-- Success Page -->
+    <!-- <div v-if="isVisible === 3" class="bg-[#227C9D] h-screen flex flex-col justify-start items-center">
+      <h2 class="text-white text-7xl mt-10 justify-start">Level 1 Completed !!</h2>
+      <img :src="loadSuccess" alt="Prize" class=" w-[610px] h-[600px] items-end">
+     </div> -->
+
+    <!-- Level-up Page -->
+    <!-- <div v-if="isVisible === 3" class="bg-[#227C9D] h-screen flex flex-col justify-between items-center">
+      <h2 class="text-white text-7xl mt-10 ">Finished Mode Easy</h2>
+      <img :src="levelSuccess" alt="Prize" class=" w-[410px] h-[400px] items-end mt-[-10px]">
+      <button class="hover:scale-120"><img :src="continueButton" alt="Prize" class=" w-[150px] h-[150px] items-end mt-[-100px]"></button>
+      <h2 class="text-white text-4xl mb-[60px] justify-end">You have received 5 additional hint.</h2>
+    </div> -->
+
+    <!-- Final Page -->
+    <!-- <div v-if="isVisible === 3" class="bg-[#227C9D] h-screen flex flex-col justify-between items-center">
+      <h2 class="text-white text-7xl mt-10 ">Congratulations !</h2>
+      <h2 class="text-white text-2xl mt-10 ">You success 100% in this game</h2>
+
+      <img :src="prizePhoto" alt="Prize" class=" w-[1500px] h-[800px] items-end mt-[-100px]">
+     </div> -->
   </div>
 </template>
+
+
 
 <style scoped>
 @import url("https://fonts.googleapis.com/css2?family=Irish+Grover&display=swap");
@@ -58,6 +207,8 @@ h1 {
   font-family: "Irish Grover", sans-serif;
   font-weight: 500;
   font-style: normal;
+  text-shadow: 2px 2px 4px rgba(0, 0, 0, 0.3);
+  /* เงาที่ตัวอักษร */
 }
 
 * {
