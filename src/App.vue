497517f461a002e73c2d7a6a6c69ccc3e30709ef
Nofmann <111362962+namfonn02@users.noreply.github.com>
2024-08-22 23:37:03 +0700

    create: create hint feature
    create hint feature but not finish yet

--- src/App.vue
+++ src/App.vue
@@ -11,7 +11,7 @@ import levelSuccess from './components/icons/level-up-photo.png'
 import prizePhoto from './components/icons/prizePhoto.png'
 import Questions from './data/word_levels.json'
 
-import { ref, computed } from 'vue'
+import { ref, computed, watch } from 'vue'
 
 const isVisible = ref(0)
 const level = ref(0)
@@ -21,6 +21,8 @@ const spaceCount = ref(0)
 const count = ref(0)
 const correctAnswer = ref([])
 const col = ref(0)
+const hints = ref(300)
+const usedHintIndexes = ref([])
 
 const modePage = () => {
   isVisible.value = 1
@@ -72,27 +74,12 @@ const selectLetter = (letter) => {
     selectedAnswer.value[count.value] = letter
     count.value += 1
 
-    if (count.value >= correctAnswer.value.length) {
-      checkAnswer()
-    }
+    // if (count.value >= correctAnswer.value.length) {
+    //   checkAnswer()
+    // }
   }
 }
 
-// const checkAnswer = () => {
-//   const isCorrect = correctAnswer.value.every(
-//     (char, index) => char === selectedAnswer.value[index]
-//   )
-
-//   if (isCorrect) {
-//     console.log('you win')
-//     level.value += 1
-//     playMode('easy')
-//     selectedAnswer.value = Array(spaceCount.value).fill(' ')
-//     count.value = 0
-//   } else {
-//     console.log('you lose')
-//   }
-// }
 
 const checkAnswer = () => {
   const flattenedSelectedAnswer = selectedAnswer.value.flat()
@@ -115,7 +102,7 @@ function shuffleArray(arr) {
   let shuffled = arr.slice() // สร้างสำเนาของอาร์เรย์
   for (let i = shuffled.length - 1; i > 0; i--) {
     const j = Math.floor(Math.random() * (i + 1))
-    ;[shuffled[i], shuffled[j]] = [shuffled[j], shuffled[i]] // สลับตำแหน่งของสมาชิกในอาร์เรย์
+      ;[shuffled[i], shuffled[j]] = [shuffled[j], shuffled[i]] // สลับตำแหน่งของสมาชิกในอาร์เรย์
   }
   return shuffled
 }
@@ -137,42 +124,49 @@ const splitWords = computed(() => {
   }
   return rows
 })
+
+const useHint = () => {
+  if (hints.value > 0) {
+    const restIndexes = Array
+      .from({ length: correctAnswer.value.length }, (_, i) => i)
+      .filter(e => !usedHintIndexes.value.includes(e));
+
+    const randomOfRest = Math.floor(Math.random() * restIndexes.length);
+    const randomIndex = restIndexes[randomOfRest];
+
+    usedHintIndexes.value.push(randomIndex);
+    selectedAnswer.value[randomIndex] = correctAnswer.value[randomIndex];
+    hints.value -= 1;
+    count.value++;
+  }
+}
+
+watch(() => count.value, () => {
+  if (count.value >= correctAnswer.value.length) {
+    checkAnswer()
+  }
+})
 </script>
 
 <template>
   <div>
     <!-- Start Page -->
-    <div
-      v-if="isVisible === 0"
-      class="bg-[#FEF9EF] flex flex-col items-center justify-center h-screen"
-    >
+    <div v-if="isVisible === 0" class="bg-[#FEF9EF] flex flex-col items-center justify-center h-screen">
       <h1 class="text-[150px] justify-start text-[#237C9D] mt-[-75px]">
         CLICK WORD
       </h1>
       <button @click="modePage">
-        <img
-          :src="playButton"
-          alt="Play Button"
-          class="w-60 h-60 mx-auto mb-[50px] mt-10 hover:scale-110"
-        />
+        <img :src="playButton" alt="Play Button" class="w-60 h-60 mx-auto mb-[50px] mt-10 hover:scale-110" />
       </button>
       <div class="flex gap-80 mt-10">
         <div class="flex flex-col item-center gap-2">
-          <img
-            :src="prize"
-            alt="Prize"
-            class="w-20 h-20 mx-auto my-auto mb-1"
-          />
+          <img :src="prize" alt="Prize" class="w-20 h-20 mx-auto my-auto mb-1" />
           <h3 class="bg-[#19C3B2] text-[#FEF9EF] text-[20px] rounded-2xl p-3">
             Success (0%)
           </h3>
         </div>
         <div class="flex flex-col item-center gap-2">
-          <img
-            :src="bulb"
-            alt="Bulb Button"
-            class="w-20 h-20 mx-auto my-auto mb-1 mr-3"
-          />
+          <img :src="bulb" alt="Bulb Button" class="w-20 h-20 mx-auto my-auto mb-1 mr-3" />
           <h3 class="bg-[#FF9090] text-[#FEF9EF] text-[20px] rounded-2xl p-3">
             Hints (3)
           </h3>
@@ -181,68 +175,40 @@ const splitWords = computed(() => {
     </div>
 
     <!-- Mode Page -->
-    <div
-      v-if="isVisible === 1"
-      class="bg-[#FEF9EF] flex flex-col items-center justify-center h-screen gap-16"
-    >
+    <div v-if="isVisible === 1" class="bg-[#FEF9EF] flex flex-col items-center justify-center h-screen gap-16">
       <h1 class="text-[130px] justify-start text-[#237C9D]">MODE</h1>
       <div class="flex flex-col gap-6">
-        <button
-          @click="playEasyMode"
-          class="bg-[#19C3B2] text-[#FEF9EF] text-[40px] rounded-2xl px-8 hover:scale-110 hover:bg-[#20a396]"
-        >
+        <button @click="playEasyMode"
+          class="bg-[#19C3B2] text-[#FEF9EF] text-[40px] rounded-2xl px-8 hover:scale-110 hover:bg-[#20a396]">
           Easy
         </button>
-        <button
-          @click="playMediumMode"
-          class="bg-[#FFCB77] text-[#FEF9EF] text-[40px] rounded-2xl px-8 hover:scale-110 hover:bg-[#ffb031]"
-        >
+        <button @click="playMediumMode"
+          class="bg-[#FFCB77] text-[#FEF9EF] text-[40px] rounded-2xl px-8 hover:scale-110 hover:bg-[#ffb031]">
           Medium
         </button>
-        <button
-          @click="playHardMode"
-          class="bg-[#FE6D73] text-[#FEF9EF] text-[40px] rounded-2xl px-8 hover:scale-110 hover:bg-[#ee464c]"
-        >
+        <button @click="playHardMode"
+          class="bg-[#FE6D73] text-[#FEF9EF] text-[40px] rounded-2xl px-8 hover:scale-110 hover:bg-[#ee464c]">
           Hard
         </button>
         <button @click="startPage">
-          <img
-            :src="back"
-            alt="Back Button"
-            class="w-20 h-20 mx-auto my-auto mb-1 mt-16 hover:scale-150"
-          />
+          <img :src="back" alt="Back Button" class="w-20 h-20 mx-auto my-auto mb-1 mt-16 hover:scale-150" />
         </button>
       </div>
     </div>
 
     <!-- Play Page -->
-    <div
-      v-if="isVisible === 2"
-      class="flex flex-col justify-between bg-[#FEF9EF] h-screen"
-    >
+    <div v-if="isVisible === 2" class="flex flex-col justify-between bg-[#FEF9EF] h-screen">
       <div class="flex justify-between items-start">
         <button @click="modePage">
-          <img
-            :src="homeButton"
-            alt="Home Button"
-            class="w-[50px] h-[50px] ml-5 mt-5 hover:scale-110"
-          />
+          <img :src="homeButton" alt="Home Button" class="w-[50px] h-[50px] ml-5 mt-5 hover:scale-110" />
         </button>
         <h3 class="mt-6 text-4xl text-black">{{ `Level ${level + 1}` }}</h3>
         <div class="flex flex-col">
           <button @click="successPage">
-            <img
-              :src="helpButton"
-              alt="Help Button"
-              class="w-[50px] h-[50px] mr-5 mt-5 hover:scale-110"
-            />
+            <img :src="helpButton" alt="Help Button" class="w-[50px] h-[50px] mr-5 mt-5 hover:scale-110" />
           </button>
           <button @click="modePage">
-            <img
-              :src="soundButton"
-              alt="Sound Button"
-              class="w-[50px] h-[50px] mr-5 mt-5 hover:scale-110"
-            />
+            <img :src="soundButton" alt="Sound Button" class="w-[50px] h-[50px] mr-5 mt-5 hover:scale-110" />
           </button>
         </div>
       </div>
@@ -270,42 +236,28 @@ const splitWords = computed(() => {
       </div> -->
 
       <div class="flex flex-col items-center justify-center flex-1 gap-8">
-        <div
-          v-for="(row, rowIndex) in splitWords"
-          :key="rowIndex"
-          class="flex flex-row gap-2 mb-8"
-        >
-          <button
-            v-for="word in row"
-            :key="word"
-            @click="selectLetter(word)"
-            class="text-[40px] text-[#FEF9EF] rounded-2xl w-20 h-20 bg-[#19C3B2]"
-          >
+        <div v-for="(row, rowIndex) in splitWords" :key="rowIndex" class="flex flex-row gap-2 mb-8">
+          <button v-for="word in row" :key="word" @click="selectLetter(word)"
+            class="text-[40px] text-[#FEF9EF] rounded-2xl w-20 h-20 bg-[#19C3B2]">
             {{ word }}
           </button>
         </div>
         <div class="flex flex-row gap-2 mb-8">
-          <div
-            v-for="(item, index) in selectedAnswer"
-            :key="index"
-            class="text-[40px] text-black flex justify-center items-center w-20 h-20 border-2 border-[#19C3B2] rounded-2xl"
-          >
+          <div v-for="(item, index) in selectedAnswer" :key="index"
+            class="text-[40px] text-black flex justify-center items-center w-20 h-20 border-2 border-[#19C3B2] rounded-2xl">
             {{ item }}
           </div>
         </div>
       </div>
 
       <div class="flex justify-center items-end mb-16 gap-10">
-        <button
-          @click="clearSelectAnswer"
-          class="bg-[#000000] text-[#FEF9EF] text-3xl rounded-xl px-8 w-56 hover:bg-[#878787] focus:bg-black"
-        >
+        <button @click="clearSelectAnswer"
+          class="bg-[#000000] text-[#FEF9EF] text-3xl rounded-xl px-8 w-56 hover:bg-[#878787] focus:bg-black">
           Clear
         </button>
-        <button
-          class="bg-[#000000] text-[#FEF9EF] text-3xl rounded-xl px-8 w-56 hover:bg-[#878787] focus:bg-black"
-        >
-          Hints ( 3 )
+        <button @click="useHint"
+          class="bg-[#000000] text-[#FEF9EF] text-3xl rounded-xl px-8 w-56 hover:bg-[#878787] focus:bg-black">
+          Hints ({{ hints }})
         </button>
       </div>
     </div>
