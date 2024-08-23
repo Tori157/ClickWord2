1ee82dbb855a26dd41cbf284f588624feb515c58
Nofmann <111362962+namfonn02@users.noreply.github.com>
2024-08-23 19:27:23 +0700

    feat: Extend Tailwind theme with custom utilities and colors for consistent styling

--- src/App.vue
+++ src/App.vue
@@ -1,4 +1,6 @@
 <script setup>
+import { ref, computed, watch } from "vue";
+
 import playButton from "./components/icons/play.png";
 import bulb from "./components/icons/bulb.png";
 import prize from "./components/icons/prize.png";
@@ -13,8 +15,6 @@ import Questions from "./data/word_levels.json";
 
 import "./extensions/array";
 
-import { ref, computed, watch } from "vue";
-
 const isVisible = ref(0);
 const filteredWordCollection = ref([]);
 const playedIds = [];
@@ -28,6 +28,7 @@ const hints = ref(3);
 const usedHintIndexes = ref([]);
 const clickedLetters = ref({});
 const onMode = ref("");
+
 
 const modePage = () => {
   isVisible.value = 1;
@@ -58,7 +59,7 @@ const nextLevel = () => {
   const question = filteredWordCollection.value[randomIndex];
   currentWordId.value = question.id;
 
-  selectedWord.value = shuffleArray(question.word.split(""));
+  selectedWord.value = question.word.split("").shuffle();
   boxAnswerLength.value = selectedWord.value.length;
   correctAnswer.value = question.correctAnswer.split("");
   selectedAnswer.value = Array(boxAnswerLength.value).fill("");
@@ -105,24 +106,14 @@ const checkAnswer = () => {
   );
 
   if (isCorrect) {
-    console.log("you win");
+
     playedIds.push(currentWordId.value);
     level.value += 1;
     nextLevel();
     selectedAnswer.value = Array(boxAnswerLength.value).fill("");
   } else {
-    console.log("you lose");
   }
 };
-
-function shuffleArray(arr) {
-  let shuffled = arr.slice();
-  for (let i = shuffled.length - 1; i > 0; i--) {
-    const j = Math.floor(Math.random() * (i + 1));
-    [shuffled[i], shuffled[j]] = [shuffled[j], shuffled[i]];
-  }
-  return shuffled;
-}
 
 const clearSelectAnswer = () => {
   usedHintIndexes.value.length = 0;
@@ -181,37 +172,22 @@ watch(
 <template>
   <div>
     <!-- Start Page -->
-    <div
-      v-if="isVisible === 0"
-      class="bg-[#FEF9EF] flex flex-col items-center justify-center h-screen"
-    >
+    <div v-if="isVisible === 0" class="bg-[#FEF9EF] flex flex-col items-center justify-center h-screen">
       <h1 class="text-[150px] justify-start text-[#237C9D] mt-[-75px]">
         CLICK WORD
       </h1>
       <button @click="modePage">
-        <img
-          :src="playButton"
-          alt="Play Button"
-          class="w-60 h-60 mx-auto mb-[50px] mt-10 hover:scale-110"
-        />
+        <img :src="playButton" alt="Play Button" class="w-60 h-60 mx-auto mb-[50px] mt-10 hover:scale-110" />
       </button>
       <div class="flex gap-80 mt-10">
         <div class="flex flex-col item-center gap-2">
-          <img
-            :src="prize"
-            alt="Prize"
-            class="w-20 h-20 mx-auto my-auto mb-1"
-          />
+          <img :src="prize" alt="Prize" class="w-20 h-20 mx-auto my-auto mb-1" />
           <h3 class="bg-[#19C3B2] text-[#FEF9EF] text-[20px] rounded-2xl p-3">
             Success (0%)
           </h3>
         </div>
         <div class="flex flex-col item-center gap-2">
-          <img
-            :src="bulb"
-            alt="Bulb Button"
-            class="w-20 h-20 mx-auto my-auto mb-1 mr-3"
-          />
+          <img :src="bulb" alt="Bulb Button" class="w-20 h-20 mx-auto my-auto mb-1 mr-3" />
           <h3 class="bg-[#FF9090] text-[#FEF9EF] text-[20px] rounded-2xl p-3">
             Hints ({{ hints }})
           </h3>
@@ -220,111 +196,68 @@ watch(
     </div>
 
     <!-- Mode Page -->
-    <div
-      v-if="isVisible === 1"
-      class="bg-[#FEF9EF] flex flex-col items-center justify-center h-screen gap-16"
-    >
+    <div v-if="isVisible === 1" class="bg-[#FEF9EF] flex flex-col items-center justify-center h-screen gap-16">
       <h1 class="text-[130px] justify-start text-[#237C9D]">MODE</h1>
       <div class="flex flex-col gap-6">
-        <button
-          @click="playOnMode('easy')"
-          class="bg-[#19C3B2] text-[#FEF9EF] text-[40px] rounded-2xl px-8 hover:scale-110 hover:bg-[#20a396]"
-        >
+        <button @click="playOnMode('easy')"
+          class="bg-[#19C3B2] text-[#FEF9EF] text-[40px] rounded-2xl px-8 hover:scale-110 hover:bg-[#20a396]">
           Easy
         </button>
-        <button
-          @click="playOnMode('medium')"
-          class="bg-[#FFCB77] text-[#FEF9EF] text-[40px] rounded-2xl px-8 hover:scale-110 hover:bg-[#ffb031]"
-        >
+        <button @click="playOnMode('medium')"
+          class="bg-[#FFCB77] text-[#FEF9EF] text-[40px] rounded-2xl px-8 hover:scale-110 hover:bg-[#ffb031]">
           Medium
         </button>
-        <button
-          @click="playOnMode('hard')"
-          class="bg-[#FE6D73] text-[#FEF9EF] text-[40px] rounded-2xl px-8 hover:scale-110 hover:bg-[#ee464c]"
-        >
+        <button @click="playOnMode('hard')"
+          class="bg-[#FE6D73] text-[#FEF9EF] text-[40px] rounded-2xl px-8 hover:scale-110 hover:bg-[#ee464c]">
           Hard
         </button>
         <button @click="startPage">
-          <img
-            :src="back"
-            alt="Back Button"
-            class="w-20 h-20 mx-auto my-auto mb-1 mt-16 hover:scale-150"
-          />
+          <img :src="back" alt="Back Button" class="w-20 h-20 mx-auto my-auto mb-1 mt-16 hover:scale-150" />
         </button>
       </div>
     </div>
 
     <!-- Play Page -->
-    <div
-      v-if="isVisible === 2"
-      class="flex flex-col justify-between bg-[#FEF9EF] h-screen"
-    >
+    <div v-if="isVisible === 2" class="flex flex-col justify-between bg-[#FEF9EF] h-screen">
       <div class="flex justify-between items-start">
         <button @click="modePage">
-          <img
-            :src="homeButton"
-            alt="Home Button"
-            class="w-[50px] h-[50px] ml-5 mt-5 hover:scale-110"
-          />
+          <img :src="homeButton" alt="Home Button" class="w-[50px] h-[50px] ml-5 mt-5 hover:scale-110" />
         </button>
         <h3 class="mt-6 text-4xl text-black">{{ `Level ${level + 1}` }}</h3>
         <div class="flex flex-col">
           <button @click="successPage">
-            <img
-              :src="helpButton"
-              alt="Help Button"
-              class="w-[50px] h-[50px] mr-5 mt-5 hover:scale-110"
-            />
+            <img :src="helpButton" alt="Help Button" class="w-[50px] h-[50px] mr-5 mt-5 hover:scale-110" />
           </button>
           <button @click="modePage">
-            <img
-              :src="soundButton"
-              alt="Sound Button"
-              class="w-[50px] h-[50px] mr-5 mt-5 hover:scale-110"
-            />
+            <img :src="soundButton" alt="Sound Button" class="w-[50px] h-[50px] mr-5 mt-5 hover:scale-110" />
           </button>
         </div>
       </div>
 
       <div class="flex flex-col items-center justify-center flex-1 gap-8">
-        <div
-          v-for="(row, rowIndex) in splitWords"
-          :key="rowIndex"
-          class="flex flex-row gap-2 mb-8"
-        >
-          <button
-            v-for="item in row"
-            :key="item.index"
-            @click="selectLetter(item.letter, item.index)"
+        <div v-for="(row, rowIndex) in splitWords" :key="rowIndex" class="flex flex-row gap-2 mb-8">
+          <button v-for="item in row" :key="item.index" @click="selectLetter(item.letter, item.index)"
             :disabled="clickedLetters[item.index]"
-            class="text-[40px] text-[#FEF9EF] rounded-2xl w-20 h-20 bg-[#19C3B2]"
-          >
-            {{ item.letter }}
+            class="text-[40px] text-[#FEF9EF] rounded-2xl w-20 h-20 bg-[#19C3B2]">
+            {{ item.letter.toUpperCase() }}
           </button>
         </div>
         <div class="flex flex-row gap-2 mt-5">
-          <div
-            v-for="(item, index) in selectedAnswer"
-            :key="index"
-            :class="[usedHintIndexes.includes(index) ? 'bg-[#FFCB77]' : '']"
-            class="text-[40px] text-black flex justify-center items-center w-20 h-20 border-2 border-[#19C3B2] rounded-2xl"
-          >
+          <div v-for="(item, index) in selectedAnswer" :key="index"
+            :class="[usedHintIndexes.includes(index) ? 'hinted-box' : !item ? 'unfilled-box' : 'filled-box']"
+            class="box-base">
             {{ item.toUpperCase() }}
           </div>
         </div>
       </div>
 
       <div class="flex justify-center items-end mb-16 gap-10">
-        <button
-          @click="clearSelectAnswer"
-          class="bg-[#000000] text-[#FEF9EF] text-3xl rounded-xl px-8 w-56 hover:bg-[#878787] focus:bg-black"
-        >
+        <button @click="clearSelectAnswer"
+          class="bg-[#000000] text-[#FEF9EF] text-3xl rounded-xl px-8 w-56 hover:bg-[#878787] focus:bg-black">
           Clear
         </button>
-        <button
-          @click="useHint"
-          class="bg-[#000000] text-[#FEF9EF] text-3xl rounded-xl px-8 w-56 hover:bg-[#878787] focus:bg-black"
-        >
+        <button @click="useHint"
+          class="bg-[#000000] text-[#FEF9EF] text-3xl rounded-xl px-8 w-56 hover:bg-[#878787] focus:bg-black">
           Hints ({{ hints }})
         </button>
       </div>
@@ -334,7 +267,7 @@ watch(
     <div v-if="isVisible === 3" class="bg-[#227C9D] h-screen flex flex-col justify-start items-center">
       <h2 class="text-white text-7xl mt-10 justify-start">Level 1 Completed !!</h2>
       <img :src="loadSuccess" alt="Prize" class=" w-[610px] h-[600px] items-end">
-     </div>
+    </div>
 
     <!-- Level-up Page -->
     <!-- <div v-if="isVisible === 3" class="bg-[#227C9D] h-screen flex flex-col justify-between items-center">
--- tailwind.config.js
+++ tailwind.config.js
@@ -5,10 +5,54 @@ export default {
     "./src/**/*.{vue,js,ts,jsx,tsx}",
   ],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        'custom-teal': '#19C3B2',
+        'custom-white': '#FFFFFF',
+        'custom-yellow': '#FFCB77',
+      },
+      borderWidth: {
+        '2': '2px',
+      },
+      borderRadius: {
+        '2xl': '1rem',
+      },
+      fontSize: {
+        '40px': '40px',
+      },
+    },
   },
   plugins: [
     require('daisyui'),
+    function ({ addUtilities }) {
+      addUtilities({
+        '.box-base': {
+          'text-align': 'center',
+          'display': 'flex',
+          'justify-content': 'center',
+          'align-items': 'center',
+          'width': '5rem',
+          'height': '5rem',
+          'border-width': '2px',
+          'border-radius': '1rem',
+          'font-size': '40px',
+        },
+        '.unfilled-box': {
+          'border-color': '#19C3B2',
+          'color': '#19C3B2',
+        },
+        '.filled-box': {
+          'background-color': '#19C3B2',
+          'color': '#FFFFFF',
+          'border-color': '#19C3B2',
+        },
+        '.hinted-box': {
+          'background-color': '#FFCB77',
+          'color': '#FFFFFF',
+          'border-color': '#19C3B2',
+        },
+      });
+    },
   ],
 }
 
