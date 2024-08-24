83d5a04aa901b285697605254307a38f395dcd60
WarisaTT <warisa.thia@mail.kmutt.ac.th>
2024-08-24 09:11:15 +0700

    Revert "Merge pull request #9 from WarisaTT/Namfon-Hint"
    This reverts commit f79e07fed38c5542f1147ad0a0f0fbb84d8d9c1b, reversing
    changes made to ccccdec648a4d924e39076e497250ebbb043d199.

--- src/App.vue
+++ src/App.vue
@@ -1,264 +1,360 @@
 <script setup>
-import { ref, computed, watch } from "vue";
+import playButton from './components/icons/play.png'
+import bulb from './components/icons/bulb.png'
+import prize from './components/icons/prize.png'
+import back from './components/icons/back.png'
+import homeButton from './components/icons/HomeButton.png'
+import helpButton from './components/icons/helpButton.png'
+import soundButton from './components/icons/soundButton.png'
+import loadSuccess from './components/icons/loadPhoto.png'
+import levelSuccess from './components/icons/level-up-photo.png'
+import prizePhoto from './components/icons/prizePhoto.png'
+import Questions from './data/word_levels.json'
 
-import playButton from "./components/icons/play.png";
-import bulb from "./components/icons/bulb.png";
-import prize from "./components/icons/prize.png";
-import back from "./components/icons/back.png";
-import homeButton from "./components/icons/HomeButton.png";
-import helpButton from "./components/icons/helpButton.png";
-import soundButton from "./components/icons/soundButton.png";
-import loadSuccess from "./components/icons/loadPhoto.png";
-import levelSuccess from "./components/icons/level-up-photo.png";
-import prizePhoto from "./components/icons/prizePhoto.png";
-import Questions from "./data/word_levels.json";
+import { ref, computed } from 'vue'
 
-import "./extensions/array";
-
-const isVisible = ref(0);
-const filteredWordCollection = ref([]);
-const playedIds = [];
-const currentWordId = ref(0);
-const level = ref(0);
-const selectedWord = ref([]);
-const selectedAnswer = ref([]);
-const correctAnswer = ref([]);
-const boxAnswerLength = ref(0);
-const hints = ref(3);
-const usedHintIndexes = ref([]);
-const clickedLetters = ref({});
-const onMode = ref("");
-
+const isVisible = ref(0)
+const level = ref(0)
+const selectedWord = ref([])
+const selectedAnswer = ref([])
+const spaceCount = ref(0)
+const count = ref(0)
+const correctAnswer = ref([])
+const col = ref(0)
+const currentMode = ref('')
 
 const modePage = () => {
-  isVisible.value = 1;
-  clearLevel();
-};
+  isVisible.value = 1
+  clearLevel()
+  count.value = 0
+}
 
 const startPage = () => {
-  isVisible.value = 0;
-};
-
-const gamePlayPage = () => {
-  isVisible.value = 2;
-};
+  isVisible.value = 0
+}
 
 const successPage = () => {
-  isVisible.value = 3;
-};
-
-const nextLevel = () => {
-  gamePlayPage();
-  clearSelectAnswer();
-
-  filteredWordCollection.value =
-    filteredWordCollection.value.filterByExcludeIds(playedIds);
-  const randomIndex = Math.floor(
-    Math.random() * filteredWordCollection.value.length,
-  );
-  const question = filteredWordCollection.value[randomIndex];
-  currentWordId.value = question.id;
-
-  selectedWord.value = question.word.split("").shuffle();
-  boxAnswerLength.value = selectedWord.value.length;
-  correctAnswer.value = question.correctAnswer.split("");
-  selectedAnswer.value = Array(boxAnswerLength.value).fill("");
-};
-
-const playOnMode = (mode) => {
-  onMode.value = mode;
-  filteredWordCollection.value = Questions.filterByMode(mode).shuffle();
-  nextLevel();
-};
-
-const selectLetter = (letter, index) => {
-  if (
-    filledBoxLength.value < selectedAnswer.value.length &&
-    !clickedLetters.value[index]
-  ) {
-    const firstEmptyIndex = selectedAnswer.value.findIndex(
-      (char) => char === "",
-    );
-    if (firstEmptyIndex !== -1) {
-      selectedAnswer.value[firstEmptyIndex] = letter;
-      clickedLetters.value[index] = true;
-    }
-  }
-};
-
-const putHintOn = (letter, correctIndex) => {
-  if (filledBoxLength.value < selectedAnswer.value.length) {
-    selectedAnswer.value[correctIndex] = letter;
-    usedHintIndexes.value.push(correctIndex);
-    const clickedIndex = splitWords.value
-      .flat()
-      .findIndex((e, i) => letter === e.letter && !clickedLetters.value[i]);
-    if (clickedIndex !== -1) {
-      clickedLetters.value[clickedIndex] = true;
-    }
-  }
-};
-
-const checkAnswer = () => {
-  const flattenedSelectedAnswer = selectedAnswer.value.flat();
-  const isCorrect = correctAnswer.value.every(
-    (char, index) => char === flattenedSelectedAnswer[index],
-  );
-
-  if (isCorrect) {
-
-    playedIds.push(currentWordId.value);
-    level.value += 1;
-    nextLevel();
-    selectedAnswer.value = Array(boxAnswerLength.value).fill("");
-  } else {
-  }
-};
+  isVisible.value = 3
+}
 
 const clearSelectAnswer = () => {
-  usedHintIndexes.value.length = 0;
-  selectedAnswer.value = Array(boxAnswerLength.value).fill("");
-  clickedLetters.value = {};
-};
+  selectedAnswer.value = Array(spaceCount.value).fill(' ')
+  count.value = 0
+}
 
 const clearLevel = () => {
-  level.value = 0;
-};
+  level.value = 0
+}
+
+const getCharClass = (index) => {
+  // ปรับการจัดการ class ตาม index หรือเงื่อนไขอื่น ๆ
+  // ใช้ class ต่าง ๆ ตาม index ของตัวอักษร
+  return index % 2 === 0
+    ? 'text-[40px] flex justify-center items-center w-20 h-20 border-2 border-[#19C3B2] rounded-2xl'
+    : 'bg-[#19C3B2] text-[40px] text-[#FFFFFF] flex justify-center items-center w-20 h-20 border-2 border-[#19C3B2] rounded-2xl'
+}
+
+const changeMode = (mode) => {
+  currentMode.value = mode
+  level.value = 0 // รีเซ็ตระดับเมื่อเปลี่ยนโหมด
+  playMode(currentMode.value) // เริ่มต้นโหมดที่เลือก
+}
+
+const playMode = (difficulty) => {
+  isVisible.value = 2
+  clearSelectAnswer()
+  // const question = Questions.filter((q) => q.difficulty === difficulty)[
+  //   level.value
+  // ]
+
+  const filterquestions = Questions.filter((q) => q.difficulty === difficulty)
+
+  const randomIndex = Math.floor(Math.random() * filterquestions.length)
+
+  const question = filterquestions[randomIndex]
+
+  selectedWord.value = shuffleArray(question.word.split(''))
+
+  spaceCount.value = selectedWord.value.length
+  col.value = selectedWord.value.length
+
+  correctAnswer.value = question.correctAnswer.split('')
+  selectedAnswer.value = Array(spaceCount.value).fill(' ')
+
+  console.log(selectedAnswer.value)
+}
+
+const selectLetter = (letter) => {
+  if (count.value < selectedAnswer.value.length) {
+    selectedAnswer.value[count.value] = letter
+    count.value += 1
+
+    if (count.value >= correctAnswer.value.length) {
+      checkAnswer()
+    }
+  }
+}
+
+// const checkAnswer = () => {
+//   const isCorrect = correctAnswer.value.every(
+//     (char, index) => char === selectedAnswer.value[index]
+//   )
+
+//   if (isCorrect) {
+//     console.log('you win')
+//     level.value += 1
+//     playMode('easy')
+//     selectedAnswer.value = Array(spaceCount.value).fill(' ')
+//     count.value = 0
+//   } else {
+//     console.log('you lose')
+//   }
+// }
+
+// const checkAnswer = () => {
+//   const flattenedSelectedAnswer = selectedAnswer.value.flat()
+//   const isCorrect = correctAnswer.value.every(
+//     (char, index) => char === flattenedSelectedAnswer[index]
+//   )
+
+//   if (isCorrect) {
+//     console.log('you win')
+//     level.value += 1
+//     playMode('easy')
+//     selectedAnswer.value = Array(spaceCount.value).fill(' ')
+//     count.value = 0
+//   } else {
+//     console.log('you lose')
+//   }
+// }
+
+const checkAnswer = () => {
+  const isCorrect = correctAnswer.value.every(
+    (char, index) => char === selectedAnswer.value[index]
+  )
+
+  if (isCorrect) {
+    console.log('you win')
+    isVisible.value = 3
+
+    // นับถอยหลัง 1 วินาที
+    setTimeout(() => {
+      level.value += 1 // เปลี่ยนไประดับถัดไป
+      playMode(currentMode.value)
+      selectedAnswer.value = Array(spaceCount.value).fill(' ') // รีเซ็ตคำตอบที่เลือก
+      count.value = 0 // รีเซ็ตตัวนับเวลา
+
+      // เปลี่ยนหน้าเพจเป็นหน้าเริ่มต้นหรือหน้าโหมดตามที่ต้องการ
+      isVisible.value = 2 // เปลี่ยนเป็นหน้าถัดไปที่ต้องการ
+    }, 1000)
+
+    // เริ่มจับเวลา
+  } else {
+    console.log('you lose')
+    // Clear array after lose
+    selectedWord.value = []
+    // นับถอยหลัง 1 วินาที
+    setTimeout(() => {
+      // level.value += 1;// เปลี่ยนไประดับถัดไป
+      playMode(currentMode.value)
+      selectedAnswer.value = Array(spaceCount.value).fill(' ') // รีเซ็ตคำตอบที่เลือก
+      count.value = 0 // รีเซ็ตตัวนับเวลา
+
+      // เปลี่ยนหน้าเพจเป็นหน้าเริ่มต้นหรือหน้าโหมดตามที่ต้องการ
+      isVisible.value = 2 // เปลี่ยนเป็นหน้าถัดไปที่ต้องการ
+    }, 1000)
+  }
+}
+
+function shuffleArray(arr) {
+  let shuffled = arr.slice() // สร้างสำเนาของอาร์เรย์
+  for (let i = shuffled.length - 1; i > 0; i--) {
+    const j = Math.floor(Math.random() * (i + 1))
+    ;[shuffled[i], shuffled[j]] = [shuffled[j], shuffled[i]] // สลับตำแหน่งของสมาชิกในอาร์เรย์
+  }
+  return shuffled
+}
 
 const splitWords = computed(() => {
-  const rows = [];
-  const perRow = Math.ceil(boxAnswerLength.value / 2);
-  selectedWord.value.forEach((letter, index) => {
-    const rowIndex = Math.floor(index / perRow);
-    if (!rows[rowIndex]) {
-      rows[rowIndex] = [];
-    }
-    rows[rowIndex].push({ letter, index });
-  });
-  return rows;
-});
-
-const filledBoxLength = computed(
-  () => selectedAnswer.value.filter((l) => /^[a-zA-Z]+$/.test(l)).length,
-);
-
-const useHint = () => {
-  if (hints.value > 0 && filledBoxLength.value < correctAnswer.value.length) {
-    const availableIndexes = Array.from(
-      { length: correctAnswer.value.length },
-      (_, i) => i,
-    ).filter((e) => !usedHintIndexes.value.includes(e));
-
-    const randomOfAvailable = Math.floor(
-      Math.random() * availableIndexes.length,
-    );
-    const randomIndex = availableIndexes[randomOfAvailable];
-
-    putHintOn(correctAnswer.value[randomIndex], randomIndex);
-    hints.value -= 1;
+  const rows = []
+  const perRow = col.value / 2
+  for (let i = 0; i < selectedWord.value.length; i += perRow) {
+    rows.push(selectedWord.value.slice(i, i + perRow))
   }
-};
-
-watch(
-  () => filledBoxLength.value,
-  () => {
-    if (filledBoxLength.value >= correctAnswer.value.length) {
-      checkAnswer();
-    }
-  },
-);
+  return rows
+})
 </script>
 
 <template>
   <div>
     <!-- Start Page -->
-    <div v-if="isVisible === 0" class="bg-[#FEF9EF] flex flex-col items-center justify-center h-screen">
+    <div
+      v-if="isVisible === 0"
+      class="bg-[#FEF9EF] flex flex-col items-center justify-center h-screen"
+    >
       <h1 class="text-[150px] justify-start text-[#237C9D] mt-[-75px]">
         CLICK WORD
       </h1>
       <button @click="modePage">
-        <img :src="playButton" alt="Play Button" class="w-60 h-60 mx-auto mb-[50px] mt-10 hover:scale-110" />
+        <img
+          :src="playButton"
+          alt="Play Button"
+          class="w-60 h-60 mx-auto mb-[50px] mt-10 hover:scale-110"
+        />
       </button>
       <div class="flex gap-80 mt-10">
         <div class="flex flex-col item-center gap-2">
-          <img :src="prize" alt="Prize" class="w-20 h-20 mx-auto my-auto mb-1" />
+          <img
+            :src="prize"
+            alt="Prize"
+            class="w-20 h-20 mx-auto my-auto mb-1"
+          />
           <h3 class="bg-[#19C3B2] text-[#FEF9EF] text-[20px] rounded-2xl p-3">
             Success (0%)
           </h3>
         </div>
         <div class="flex flex-col item-center gap-2">
-          <img :src="bulb" alt="Bulb Button" class="w-20 h-20 mx-auto my-auto mb-1 mr-3" />
+          <img
+            :src="bulb"
+            alt="Bulb Button"
+            class="w-20 h-20 mx-auto my-auto mb-1 mr-3"
+          />
           <h3 class="bg-[#FF9090] text-[#FEF9EF] text-[20px] rounded-2xl p-3">
-            Hints ({{ hints }})
+            Hints (3)
           </h3>
         </div>
       </div>
     </div>
 
     <!-- Mode Page -->
-    <div v-if="isVisible === 1" class="bg-[#FEF9EF] flex flex-col items-center justify-center h-screen gap-16">
+    <div
+      v-if="isVisible === 1"
+      class="bg-[#FEF9EF] flex flex-col items-center justify-center h-screen gap-16"
+    >
       <h1 class="text-[130px] justify-start text-[#237C9D]">MODE</h1>
       <div class="flex flex-col gap-6">
-        <button @click="playOnMode('easy')"
-          class="bg-[#19C3B2] text-[#FEF9EF] text-[40px] rounded-2xl px-8 hover:scale-110 hover:bg-[#20a396]">
+        <button
+          @click="changeMode('easy')"
+          class="bg-[#19C3B2] text-[#FEF9EF] text-[40px] rounded-2xl px-8 hover:scale-110 hover:bg-[#20a396]"
+        >
           Easy
         </button>
-        <button @click="playOnMode('medium')"
-          class="bg-[#FFCB77] text-[#FEF9EF] text-[40px] rounded-2xl px-8 hover:scale-110 hover:bg-[#ffb031]">
+        <button
+          @click="changeMode('medium')"
+          class="bg-[#FFCB77] text-[#FEF9EF] text-[40px] rounded-2xl px-8 hover:scale-110 hover:bg-[#ffb031]"
+        >
           Medium
         </button>
-        <button @click="playOnMode('hard')"
-          class="bg-[#FE6D73] text-[#FEF9EF] text-[40px] rounded-2xl px-8 hover:scale-110 hover:bg-[#ee464c]">
+        <button
+          @click="changeMode('hard')"
+          class="bg-[#FE6D73] text-[#FEF9EF] text-[40px] rounded-2xl px-8 hover:scale-110 hover:bg-[#ee464c]"
+        >
           Hard
         </button>
         <button @click="startPage">
-          <img :src="back" alt="Back Button" class="w-20 h-20 mx-auto my-auto mb-1 mt-16 hover:scale-150" />
+          <img
+            :src="back"
+            alt="Back Button"
+            class="w-20 h-20 mx-auto my-auto mb-1 mt-16 hover:scale-150"
+          />
         </button>
       </div>
     </div>
 
     <!-- Play Page -->
-    <div v-if="isVisible === 2" class="flex flex-col justify-between bg-[#FEF9EF] h-screen">
+    <div
+      v-if="isVisible === 2"
+      class="flex flex-col justify-between bg-[#FEF9EF] h-screen"
+    >
       <div class="flex justify-between items-start">
         <button @click="modePage">
-          <img :src="homeButton" alt="Home Button" class="w-[50px] h-[50px] ml-5 mt-5 hover:scale-110" />
+          <img
+            :src="homeButton"
+            alt="Home Button"
+            class="w-[50px] h-[50px] ml-5 mt-5 hover:scale-110"
+          />
         </button>
         <h3 class="mt-6 text-4xl text-black">{{ `Level ${level + 1}` }}</h3>
         <div class="flex flex-col">
           <button @click="successPage">
-            <img :src="helpButton" alt="Help Button" class="w-[50px] h-[50px] mr-5 mt-5 hover:scale-110" />
+            <img
+              :src="helpButton"
+              alt="Help Button"
+              class="w-[50px] h-[50px] mr-5 mt-5 hover:scale-110"
+            />
           </button>
           <button @click="modePage">
-            <img :src="soundButton" alt="Sound Button" class="w-[50px] h-[50px] mr-5 mt-5 hover:scale-110" />
+            <img
+              :src="soundButton"
+              alt="Sound Button"
+              class="w-[50px] h-[50px] mr-5 mt-5 hover:scale-110"
+            />
           </button>
         </div>
       </div>
 
-      <div class="flex flex-col items-center justify-center flex-1 gap-8">
-        <div v-for="(row, rowIndex) in splitWords" :key="rowIndex" class="flex flex-row gap-2 mb-8">
-          <button v-for="item in row" :key="item.index" @click="selectLetter(item.letter, item.index)"
-            :disabled="clickedLetters[item.index]"
-            class="text-[40px] text-[#FEF9EF] rounded-2xl w-20 h-20 bg-[#19C3B2]">
-            {{ item.letter.toUpperCase() }}
+      <!-- <div class="flex flex-col items-center justify-center flex-1 gap-8">
+        <div class="flex flex-row gap-2 mb-8">
+          <button
+            v-for="word in selectedWord"
+            :key="word"
+            @click="selectLetter(word)"
+            class="text-[40px] text-[#FEF9EF] rounded-2xl w-20 h-20 bg-[#19C3B2]"
+          >
+            {{ word }}
           </button>
         </div>
+        <div class="flex flex-row gap-2 mb-8">
+          <div
+            v-for="item in selectedAnswer"
+            :key="item"
+            class="text-[40px] text-black flex justify-center items-center w-20 h-20 border-2 border-[#19C3B2] rounded-2xl"
+          >
+            {{ item }}
+          </div>
+        </div>
+      </div> -->
+
+      <div class="flex flex-col items-center justify-center">
+        <div class="flex flex-col gap-3 mb-10">
+          <div
+            v-for="(row, rowIndex) in splitWords"
+            :key="rowIndex"
+            class="flex flex-row gap-2"
+          >
+            <button
+              v-for="word in row"
+              :key="word"
+              @click="selectLetter(word)"
+              class="text-[40px] text-[#FEF9EF] rounded-2xl w-20 h-20 bg-[#19C3B2]"
+            >
+              {{ word.toUpperCase() }}
+            </button>
+          </div>
+        </div>
         <div class="flex flex-row gap-2 mt-5">
-          <div v-for="(item, index) in selectedAnswer" :key="index"
-            :class="[usedHintIndexes.includes(index) ? 'hinted-box' : !item ? 'unfilled-box' : 'filled-box']"
-            class="box-base">
+          <div
+            v-for="(item, index) in selectedAnswer"
+            :key="index"
+            class="border-[#19C3B2]"
+            :class="getCharClass(item)"
+          >
             {{ item.toUpperCase() }}
           </div>
         </div>
       </div>
 
       <div class="flex justify-center items-end mb-16 gap-10">
-        <button @click="clearSelectAnswer"
-          class="bg-[#000000] text-[#FEF9EF] text-3xl rounded-xl px-8 w-56 hover:bg-[#878787] focus:bg-black">
+        <button
+          @click="clearSelectAnswer"
+          class="bg-[#000000] text-[#FEF9EF] text-3xl rounded-xl px-8 w-56 hover:bg-[#878787] focus:bg-black"
+        >
           Clear
         </button>
-        <button @click="useHint"
-          class="bg-[#000000] text-[#FEF9EF] text-3xl rounded-xl px-8 w-56 hover:bg-[#878787] focus:bg-black">
-          Hints ({{ hints }})
+        <button
+          class="bg-[#000000] text-[#FEF9EF] text-3xl rounded-xl px-8 w-56 hover:bg-[#878787] focus:bg-black"
+        >
+          Hints ( 3 )
         </button>
       </div>
     </div>
@@ -267,7 +363,7 @@ watch(
     <div v-if="isVisible === 3" class="bg-[#227C9D] h-screen flex flex-col justify-start items-center">
       <h2 class="text-white text-7xl mt-10 justify-start">Level 1 Completed !!</h2>
       <img :src="loadSuccess" alt="Prize" class=" w-[610px] h-[600px] items-end">
-    </div>
+     </div>
 
     <!-- Level-up Page -->
     <!-- <div v-if="isVisible === 3" class="bg-[#227C9D] h-screen flex flex-col justify-between items-center">
@@ -288,11 +384,11 @@ watch(
 </template>
 
 <style scoped>
-@import url("https://fonts.googleapis.com/css2?family=Irish+Grover&display=swap");
-@import url("https://fonts.googleapis.com/css2?family=Itim&display=swap");
+@import url('https://fonts.googleapis.com/css2?family=Irish+Grover&display=swap');
+@import url('https://fonts.googleapis.com/css2?family=Itim&display=swap');
 
 h1 {
-  font-family: "Irish Grover", sans-serif;
+  font-family: 'Irish Grover', sans-serif;
   font-weight: 500;
   font-style: normal;
   text-shadow: 2px 2px 4px rgba(0, 0, 0, 0.3);
@@ -300,7 +396,7 @@ h1 {
 }
 
 * {
-  font-family: "Itim", cursive;
+  font-family: 'Itim', cursive;
   font-weight: 400;
   font-style: normal;
 }
