7161f3890cf63f2cc7adf49d6d4ffd355cbc48cb
WarisaTT <warisa.thia@mail.kmutt.ac.th>
2024-08-24 09:11:07 +0700

    Revert "Merge pull request #10 from WarisaTT/Garfairdummn"
    This reverts commit 413f701c3b06b791322cd195bd19fa23d36abc65, reversing
    changes made to f79e07fed38c5542f1147ad0a0f0fbb84d8d9c1b.

--- src/App.vue
+++ src/App.vue
@@ -30,7 +30,6 @@ const clickedLetters = ref({});
 const onMode = ref("");
 
 
-
 const modePage = () => {
   isVisible.value = 1;
   clearLevel();
@@ -266,7 +265,7 @@ watch(
 
     <!-- Success Page -->
     <div v-if="isVisible === 3" class="bg-[#227C9D] h-screen flex flex-col justify-start items-center">
-      <h2 class="text-white text-7xl mt-10 justify-start">{{ `Level ${level + 1} complete!!!` }}</h2>
+      <h2 class="text-white text-7xl mt-10 justify-start">Level 1 Completed !!</h2>
       <img :src="loadSuccess" alt="Prize" class=" w-[610px] h-[600px] items-end">
     </div>
 
