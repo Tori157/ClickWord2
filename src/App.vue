3d719886418f64c36de3c08a12f622a0654dca66
WarisaTT <warisa.thia@mail.kmutt.ac.th>
2024-08-24 20:56:14 +0700

    Update App.vue

--- src/App.vue
+++ src/App.vue
@@ -191,37 +191,22 @@ watch(
 <template>
   <div>
     <!-- Start Page -->
-    <div
-      v-if="isVisible === 0"
-      class="bg-[#FEF9EF] flex flex-col items-center justify-center h-screen"
-    >
+    <div v-if="isVisible === 0" class="bg-[#FEF9EF] flex flex-col items-center justify-center h-screen">
       <h1 class="text-[150px] justify-start text-[#237C9D] mt-[-75px]">
         CLICK WORD
       </h1>
       <button @click="modePage">
-        <img
-          :src="playButton"
-          alt="Play Button"
-          class="w-60 h-60 mx-auto mb-[50px] mt-10 hover:scale-110"
-        />
+        <img :src="playButton" alt="Play Button" class="w-60 h-60 mx-auto mb-[50px] mt-10 hover:scale-110" />
       </button>
       <div class="flex gap-80 mt-10">
         <div class="flex flex-col item-center gap-2">
-          <img
-            :src="prize"
-            alt="Prize"
-            class="w-20 h-20 mx-auto my-auto mb-1"
-          />
+          <img :src="prize" alt="Prize" class="w-20 h-20 mx-auto my-auto mb-1" />
           <h3 class="bg-[#19C3B2] text-[#FEF9EF] text-[20px] rounded-2xl p-3">
             Success (0%)
           </h3>
         </div>
         <div class="flex flex-col item-center gap-2">
-          <img
-            :src="bulb"
-            alt="Bulb Button"
-            class="w-20 h-20 mx-auto my-auto mb-1 mr-3"
-          />
+          <img :src="bulb" alt="Bulb Button" class="w-20 h-20 mx-auto my-auto mb-1 mr-3" />
           <h3 class="bg-[#FF9090] text-[#FEF9EF] text-[20px] rounded-2xl p-3">
             Hints ({{ hints }})
           </h3>
@@ -230,84 +215,48 @@ watch(
     </div>
 
     <!-- Mode Page -->
-    <div
-      v-if="isVisible === 1"
-      class="bg-[#FEF9EF] flex flex-col items-center justify-center h-screen gap-16"
-    >
+    <div v-if="isVisible === 1" class="bg-[#FEF9EF] flex flex-col items-center justify-center h-screen gap-16">
       <h1 class="text-[130px] justify-start text-[#237C9D]">MODE</h1>
       <div class="flex flex-col gap-6">
-        <button
-          @click="playOnMode('easy')"
-          class="bg-[#19C3B2] text-[#FEF9EF] text-[40px] rounded-2xl px-8 hover:scale-110 hover:bg-[#20a396]"
-        >
+        <button @click="playOnMode('easy')"
+          class="bg-[#19C3B2] text-[#FEF9EF] text-[40px] rounded-2xl px-8 hover:scale-110 hover:bg-[#20a396]">
           Easy
         </button>
-        <button
-          @click="playOnMode('medium')"
-          class="bg-[#FFCB77] text-[#FEF9EF] text-[40px] rounded-2xl px-8 hover:scale-110 hover:bg-[#ffb031]"
-        >
+        <button @click="playOnMode('medium')"
+          class="bg-[#FFCB77] text-[#FEF9EF] text-[40px] rounded-2xl px-8 hover:scale-110 hover:bg-[#ffb031]">
           Medium
         </button>
-        <button
-          @click="playOnMode('hard')"
-          class="bg-[#FE6D73] text-[#FEF9EF] text-[40px] rounded-2xl px-8 hover:scale-110 hover:bg-[#ee464c]"
-        >
+        <button @click="playOnMode('hard')"
+          class="bg-[#FE6D73] text-[#FEF9EF] text-[40px] rounded-2xl px-8 hover:scale-110 hover:bg-[#ee464c]">
           Hard
         </button>
         <button @click="startPage">
-          <img
-            :src="back"
-            alt="Back Button"
-            class="w-20 h-20 mx-auto my-auto mb-1 mt-16 hover:scale-150"
-          />
+          <img :src="back" alt="Back Button" class="w-20 h-20 mx-auto my-auto mb-1 mt-16 hover:scale-150" />
         </button>
       </div>
     </div>
 
     <!-- Play Page -->
-    <div
-      v-if="isVisible === 2"
-      class="flex flex-col justify-between bg-[#FEF9EF] h-screen"
-    >
+    <div v-if="isVisible === 2" class="flex flex-col justify-between bg-[#FEF9EF] h-screen">
       <div class="flex justify-between items-start">
         <button @click="modePage">
-          <img
-            :src="homeButton"
-            alt="Home Button"
-            class="w-[50px] h-[50px] ml-5 mt-5 hover:scale-110"
-          />
+          <img :src="homeButton" alt="Home Button" class="w-[50px] h-[50px] ml-5 mt-5 hover:scale-110" />
         </button>
         <h3 class="mt-6 text-4xl text-black">{{ `Level ${level + 1}` }}</h3>
         <div class="flex flex-col">
           <button @click="successPage">
-            <img
-              :src="helpButton"
-              alt="Help Button"
-              class="w-[50px] h-[50px] mr-5 mt-5 hover:scale-110"
-            />
+            <img :src="helpButton" alt="Help Button" class="w-[50px] h-[50px] mr-5 mt-5 hover:scale-110" />
           </button>
           <button @click="modePage">
-            <img
-              :src="soundButton"
-              alt="Sound Button"
-              class="w-[50px] h-[50px] mr-5 mt-5 hover:scale-110"
-            />
+            <img :src="soundButton" alt="Sound Button" class="w-[50px] h-[50px] mr-5 mt-5 hover:scale-110" />
           </button>
         </div>
       </div>
 
       <div class="flex flex-col items-center justify-center">
-        <div
-          v-for="(row, rowIndex) in splitWords"
-          :key="rowIndex"
-          class="flex flex-row gap-2 mb-8"
-        >
-          <button
-            v-for="item in row"
-            :key="item.index"
-            @click="selectLetter(item.letter, item.index)"
-            :disabled="clickedLetters[item.index]"
-            :class="[
+        <div v-for="(row, rowIndex) in splitWords" :key="rowIndex" class="flex flex-row gap-2 mb-8">
+          <button v-for="item in row" :key="item.index" @click="selectLetter(item.letter, item.index)"
+            :disabled="clickedLetters[item.index]" :class="[
               'text-[40px]',
               'text-[#FEF9EF]',
               'rounded-2xl',
@@ -315,81 +264,53 @@ watch(
               'h-20',
               'hover:bg-[#09897c]',
               clickedLetters[item.index] ? 'bg-[#09897c]' : 'bg-[#19C3B2]',
-            ]"
-          >
+            ]">
             {{ item.letter.toUpperCase() }}
           </button>
         </div>
         <div class="flex flex-row gap-2 mt-10">
-          <div
-            v-for="(item, index) in selectedAnswer"
-            :key="index"
-            :class="[
-              usedHintIndexes.includes(index)
-                ? 'hinted-box'
-                : !item
-                  ? 'unfilled-box'
-                  : 'filled-box',
-            ]"
-            class="box-base"
-          >
+          <div v-for="(item, index) in selectedAnswer" :key="index" :class="[
+            usedHintIndexes.includes(index)
+              ? 'hinted-box'
+              : !item
+                ? 'unfilled-box'
+                : 'filled-box',
+          ]" class="box-base">
             {{ item.toUpperCase() }}
           </div>
         </div>
       </div>
 
       <div class="flex justify-center items-end mb-16 gap-10">
-        <button
-          @click="clearSelectAnswer"
-          class="bg-[#000000] text-[#FEF9EF] text-3xl rounded-xl px-8 w-56 hover:bg-[#878787] focus:bg-black"
-        >
+        <button @click="clearSelectAnswer"
+          class="bg-[#000000] text-[#FEF9EF] text-3xl rounded-xl px-8 w-56 hover:bg-[#878787] focus:bg-black">
           Clear
         </button>
-        <button
-          @click="useHint"
-          class="bg-[#000000] text-[#FEF9EF] text-3xl rounded-xl px-8 w-56 hover:bg-[#878787] focus:bg-black"
-        >
+        <button @click="useHint"
+          class="bg-[#000000] text-[#FEF9EF] text-3xl rounded-xl px-8 w-56 hover:bg-[#878787] focus:bg-black">
           Hints ({{ hints }})
         </button>
       </div>
     </div>
 
     <!-- Success Page -->
-    <div
-      v-if="isVisible === 3"
-      class="bg-[#227C9D] h-screen flex flex-col justify-start items-center"
-    >
+    <div v-if="isVisible === 3" class="bg-[#227C9D] h-screen flex flex-col justify-start items-center">
       <h2 class="text-white text-7xl mt-10 justify-start">
         Level 1 Completed !!
       </h2>
-      <img
-        :src="loadSuccess"
-        alt="Prize"
-        class="w-[610px] h-[600px] items-end"
-      />
+      <img :src="loadSuccess" alt="Prize" class="w-[610px] h-[600px] items-end" />
     </div>
 
     <!-- Level-up Page -->
-    <div
-      v-if="isVisible === 4"
-      class="bg-[#227C9D] h-screen flex flex-col justify-between items-center"
-    >
+    <div v-if="isVisible === 4" class="bg-[#227C9D] h-screen flex flex-col justify-between items-center">
       <h2 class="text-white text-7xl mt-10">
         {{
           `Finished Mode ${onMode.charAt(0).toUpperCase() + onMode.slice(1)}`
         }}
       </h2>
-      <img
-        :src="levelSuccess"
-        alt="Prize"
-        class="w-[410px] h-[400px] items-end mt-[-10px]"
-      />
+      <img :src="levelSuccess" alt="Prize" class="w-[410px] h-[400px] items-end mt-[-10px]" />
       <button @click="modePage" class="hover:scale-150">
-        <img
-          :src="continueButton"
-          alt="Continue"
-          class="w-[150px] h-[150px] items-end mt-[-100px] hover:scale-110"
-        />
+        <img :src="continueButton" alt="Continue" class="w-[150px] h-[150px] items-end mt-[-100px] hover:scale-110" />
       </button>
       <h2 class="text-white text-4xl mb-[60px] justify-end">
         You have received 5 additional hint.
@@ -425,10 +346,12 @@ h1 {
 }
 
 .incorrect-box {
-  background-color: #ff6b6b; /* Red color */
+  background-color: #ff6b6b;
+  /* Red color */
 }
 
 .correct-box {
-  background-color: #28a745; /* Green color (optional) */
+  background-color: #28a745;
+  /* Green color (optional) */
 }
 </style>
