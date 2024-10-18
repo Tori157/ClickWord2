1622c19e7e19580402afcbd4eafd02a5604e29b3
Tori157 <puttinat2546@gmail.com>
2024-10-19 02:13:37 +0700

    112 - [FEATURE] - Add Market Modal
    - Add picture for market
    - Add Market Modal

--- src/components/AppControlPanel.vue
+++ src/components/AppControlPanel.vue
@@ -1,7 +1,19 @@
 <script setup>
+import MarketModal from './Modal/MarketModal.vue';
 import helpButton from '/public/assets/icons/helpButton.png';
 import soundButton from '/public/assets/icons/soundButton.png';
+import marketButton from '/public/assets/icons/MarketButton.png';
+import { ref } from 'vue'; // ใช้ ref เพื่อสร้าง state
 
+const isOpenMarket = ref(false); // สร้าง state สำหรับเปิด/ปิด modal
+
+const openMarketModal = () => {
+  isOpenMarket.value = true;
+};
+
+const closeMarketModal = () => {
+  isOpenMarket.value = false;
+};
 defineProps({
   openTutorialModal: {
     type: Function,
@@ -9,10 +21,17 @@ defineProps({
   },
 });
 </script>
-
+A
 <template>
   <div id="app-control-panel" class="z-50 absolute top-0 right-0">
     <div class="flex flex-col">
+      <button @click="openMarketModal">
+        <img
+          :src="marketButton"
+          alt="Market Button"
+          class="w-[50px] h-[50px] mr-5 mt-5 transition duration-300 ease-in-out transform hover:scale-110"
+        />
+      </button>
       <button @click="openTutorialModal">
         <img
           :src="helpButton"
@@ -30,4 +49,5 @@ defineProps({
       </button>
     </div>
   </div>
+  <MarketModal :isOpenMarket="isOpenMarket" :onCloseMarket="closeMarketModal" />
 </template>
