61b5ad73798d03ac755c90bdd78e08c8a82f999c
Tori157 <puttinat2546@gmail.com>
2024-10-20 18:35:44 +0700

    112 - [FUETURE] - Add Coin to Market
    - Add coin in store to market
    - Add BuyItem
    - Change hint in homepage to hintstore

--- src/views/Home/HomePage.vue
+++ src/views/Home/HomePage.vue
@@ -5,12 +5,15 @@ import BulbIcon from '/public/assets/icons/bulb.png';
 import TimeIcon from '/public/assets/icons/clock.png';
 import Ranking from '/public/assets/icons/ranking.png';
 import TrophyIcon from '/public/assets/icons/prize.png';
-
+import { useHintStore } from '@/stores';
 const titleGame1 = ['c', 'l', 'i', 'c', 'k'];
 const titleGame2 = ['w', 'o', 'r', 'd'];
 
 const success = ref(Number(localStorage.getItem('userSuccess')) ?? 0);
-const hints = ref(localStorage.getItem('hint') || 3);
+const hintStore = useHintStore();
+// มาเปลี่ยนด้วยนะจุ้บๆ
+// const hints = ref(localStorage.getItem('hint') || 3);
+
 const totalTimes = ref(localStorage.getItem('timerHistory') || 0);
 
 function formatTime(seconds) {
@@ -82,7 +85,7 @@ function formatTime(seconds) {
       <div class="flex flex-col item-center gap-2">
         <div class="flex flex-col gap-0">
           <img :src="BulbIcon" alt="Bulb Button" class="w-20 h-20 md:w-16 md:h-16 mx-auto mb-1" />
-          <h3 class="text-black text-[20px] rounded-2xl p-3 mt-[-10px]">Hints ({{ hints }})</h3>
+          <h3 class="text-black text-[20px] rounded-2xl p-3 mt-[-10px]">Hints ({{ hintStore.hint }})</h3>
         </div>
         <!-- <button class="bg-[#FF9090] text-[#FEF9EF] text-[20px] rounded-2xl p-3 hover:scale-110">Market</button> -->
       </div>
