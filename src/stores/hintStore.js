d05e3a23128c479a7329a59fc87e67182119b203
namfonn02 <Lalita.benj@kmutt.ac.th>
2024-10-22 20:37:29 +0700

    115 - [ REFACTOR ] - Use composition API in stores

--- src/stores/hintStore.js
+++ src/stores/hintStore.js
@@ -1,28 +1,24 @@
+import { ref, computed } from 'vue';
 import { defineStore, acceptHMRUpdate } from 'pinia';
 
 const HINT_KEY_STORAGE = 'hint';
 
-export const useHintStore = defineStore('hint', {
-  state: () => ({
-    hint: Number(localStorage.getItem(HINT_KEY_STORAGE)) || 1000,
-  }),
+export const useHintStore = defineStore('hint', () => {
+  const hint = ref(Number(localStorage.getItem(HINT_KEY_STORAGE)) || 1000);
 
-  actions: {
-    increment(number = 1) {
-      this.hint += number;
-      localStorage.setItem(HINT_KEY_STORAGE, this.hint);
-    },
-    decrement(number = 1) {
-      this.hint -= number;
-      localStorage.setItem(HINT_KEY_STORAGE, this.hint);
-    },
-  },
+  const isEmpty = computed(() => hint.value === 0);
 
-  getters: {
-    isEmpty() {
-      return this.hint === 0;
-    },
-  },
+  const increment = (number = 1) => {
+    hint.value += number;
+    localStorage.setItem(HINT_KEY_STORAGE, hint.value);
+  };
+
+  const decrement = (number = 1) => {
+    hint.value -= number;
+    localStorage.setItem(HINT_KEY_STORAGE, hint.value);
+  };
+
+  return { hint, isEmpty, increment, decrement };
 });
 
 if (import.meta.hot) {
